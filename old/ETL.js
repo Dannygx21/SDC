b173956d03ef910abbc1824ee252121e80bcb4ff
Daniel Gonzalez <dboyg123.dg@gmail.com>
2025-12-04 09:47:30 -0500

    working solution, CSV files Need header line

--- old/ETL.js
+++ old/ETL.js
@@ -2,14 +2,17 @@ const path = require('path')
 const dataPath = path.join(__dirname, '../Data')
 
 require('dotenv').config()
-const { DB_URL, DB_USER, DB_PASS } = process.env
+const { DB_URL2, DB_USER, DB_PASS } = process.env
 
 const fs = require('fs');
 const csv = require('csv-parser'); // You might need to install 'csv-parser' package: npm install csv-parser
 const { MongoClient } = require('mongodb'); // Assuming you're using the native MongoDB driver
 
-async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mongoUrl = DB_URL) {
-    console.log(DB_URL)
+async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mongoUrl = DB_URL2) {
+    console.log('URL: ', DB_URL2)
+    console.log('Database Name: ', dbName)
+    console.log('collection Name: ', collectionName)
+    console.log('file path: ', csvFilePath)
     const client = new MongoClient(mongoUrl);
 
     try {
@@ -19,8 +22,11 @@ async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mo
 
         const records = [];
         fs.createReadStream(csvFilePath)
+            .on('error', (err) => console.log('error :', err))
             .pipe(csv())
-            .on('data', (data) => records.push(data))
+            .on('data', (data) => {
+                records.push(data)
+            })
             .on('end', async () => {
                 if (records.length > 0) {
                     await collection.insertMany(records);
@@ -36,4 +42,4 @@ async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mo
 }
 
 // Example usage:
-importCsvProgrammatically('Catwalk-old', 'Features', path.resolve(dataPath, 'Product', 'Copy of features.csv'), DB_URL);
+importCsvProgrammatically('Catwalk-old', 'Products', '../SDC/Data/Product/Copy of product.csv', DB_URL2);
