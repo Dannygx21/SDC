5f622e887fb01aad68203f273b0a974ee9ae708b
Daniel Gonzalez <dboyg123.dg@gmail.com>
2025-12-04 13:49:06 -0500

    used to import, not perfect

--- old/ETL.js
+++ old/ETL.js
@@ -14,26 +14,31 @@ async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mo
     console.log('collection Name: ', collectionName)
     console.log('file path: ', csvFilePath)
     const client = new MongoClient(mongoUrl);
-
+    let count = 0
     try {
         await client.connect();
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
-        const records = [];
         fs.createReadStream(csvFilePath)
             .on('error', (err) => console.log('error :', err))
             .pipe(csv())
-            .on('data', (data) => {
-                records.push(data)
+            .on('data', async (data) => {
+                if (count <= 1000) {
+
+                    await collection.insertOne(data)
+                    count++
+                }
             })
             .on('end', async () => {
-                if (records.length > 0) {
-                    await collection.insertMany(records);
-                    console.log(`${records.length} records imported successfully.`);
-                } else {
-                    console.log('No records found in CSV to import.');
-                }
+                // if (records.length > 0) {
+                // await collection.insertMany(records);
+                // console.log(`${records.length} records imported successfully.`);
+                // } else {
+                // console.log('No records found in CSV to import.');
+                // }
+                console.log('Operation ended')
+                count = 0
                 await client.close();
             });
     } catch (error) {
@@ -42,4 +47,8 @@ async function importCsvProgrammatically(dbName, collectionName, csvFilePath, mo
 }
 
 // Example usage:
-importCsvProgrammatically('Catwalk-old', 'Products', '../SDC/Data/Product/Copy of product.csv', DB_URL2);
+importCsvProgrammatically('Catwalk-old', 'Related', '../SDC/Data/Product/Copy of related.csv', DB_URL2);
+importCsvProgrammatically('Catwalk-old', 'Product', '../SDC/Data/Product/Copy of product.csv', DB_URL2);
+importCsvProgrammatically('Catwalk-old', 'Features', '../SDC/Data/Product/Copy of features.csv', DB_URL2);
+importCsvProgrammatically('Catwalk-old', 'SKUs', '../SDC/Data/Product/Copy of skus.csv', DB_URL2);
+importCsvProgrammatically('Catwalk-old', 'Styles', '../SDC/Data/Product/Copy of styles.csv', DB_URL2);
